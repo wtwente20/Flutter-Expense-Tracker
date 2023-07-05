d195c9a7970012f0b65845bfa7489c8cc0a86c03
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-07-05 18:07:22 -0500

    + button leads to modal now, awaiting save functionality

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -20,13 +21,20 @@ class _ExpensesState extends State<Expenses> {
         category: Category.work),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Expense Tracker'),
+        title: const Text('Flutter Expense Tracker'),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.add),)
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: Icon(Icons.add),
+          )
         ],
       ),
       body: Column(
