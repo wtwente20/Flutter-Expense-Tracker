fc1b69b5c6eabe62596705ea4638f328ae1b3886
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-07-05 16:55:20 -0500

    updated expense.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 final uuid = Uuid();
 
@@ -8,6 +12,13 @@ enum Category {
   leisure,
   work
 }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.work: Icons.work,
+  Category.leisure: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -22,4 +33,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate{
+    return formatter.format(date);
+  }
 }
