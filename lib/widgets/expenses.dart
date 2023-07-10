d6ed35566502addc429f6566d1eb51ff11a2c132
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-07-10 18:39:34 -0500

    ability to swipe an expense to remove it from the expenses list has been added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -35,6 +35,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +57,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,),
           ),
         ],
       ),
