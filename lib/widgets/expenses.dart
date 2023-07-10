9ddd273118eaa080239a3ec7be3d180cfc6a050e
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-07-10 18:20:08 -0500

    able to add new expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -22,7 +22,16 @@ class _ExpensesState extends State<Expenses> {
   ];
 
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
